0c2af61be8f25af71107ad5adc5ba323cf9b3856
bld0156 <bld0156@unt.edu>
2020-10-23 15:53:28 -0500

    added the array size for command history

--- src/built_in_cmd.c
+++ src/built_in_cmd.c
@@ -10,14 +10,25 @@ void init_data_structures()
 	// myhistory
 	history = malloc(20*sizeof(struct command *));
 	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
-	history[0] = NULL;
+	history[1] = NULL;
+	history[2] = NULL;
+	history[3] = NULL;
+	history[4] = NULL;
+	history[5] = NULL;
+	history[6] = NULL;
+	history[7] = NULL;
+	history[8] = NULL;
+	history[9] = NULL;
+	history[10] = NULL;
+	history[11] = NULL;
+	history[12] = NULL;
+	history[13] = NULL;
+	history[14] = NULL;
+	history[15] = NULL;
+	history[16] = NULL;
+	history[17] = NULL;
+	history[18] = NULL;
+	history[19] = NULL;
 	history_size = 0;
 	// alias
 	aliases = malloc(sizeof(struct alias *));
