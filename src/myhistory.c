f8a1976e06024f56fcceb288c4a1c9a8cfbeab0d
bld0156 <brandondumas@my.unt.edu>
2020-11-07 17:42:28 -0600

    added the functionality for the myhistory command with options

--- src/myhistory.c
+++ src/myhistory.c
@@ -2,10 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include "built_in_cmd.h"
+#include "parse.h"
+#include "statement.h"
 
 void myhistory(struct command *cmd)
 {
-	printf("This is the myhistory command.\n");
+	//printf("This is the myhistory command.\n");
+	//if num args =1 print history
+	if(cmd->num_args == 1)
+	{
+	    for(int i = history_size - 1; i >=0; i--)
+	    {
+	        printf("%s\n",history[i]);
+	    }
+	}
+	//if num args =2 and arg2 = -c clear history
+	else if((cmd->num_args == 2) && !(strcmp(cmd->args[1],"-c")))
+	{
+	    for(int i = history_size - 1; i >= 0; i--)
+	    {
+	        free(history[i]);
+	        history[i] = NULL;
+	    }
+	    history_size = 0;
+	}
+	//if num = 3 arg2 = -e, and arg3 is a number 1-20 parse history[arg3-1] and pass to execute statement
+	else if((cmd->num_args == 3) && !(strcmp(cmd->args[1],"-e")))
+	{
+	    int num = stoi(cmd->args[2]);
+	    struct *statement stmt = parse(history[num]);
+	    execute_statement(stmt);
+	    delete_statement(stmt);
+	}
 }
 
 void history_add(char *entry)
