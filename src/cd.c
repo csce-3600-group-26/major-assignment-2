b2255a6731028311e8568a8132c7e365063f6866
jtw0229 <jasminewicker@my.unt.edu>
2020-11-19 11:55:25 -0600

    Implement built-in cd command

--- src/cd.c
+++ src/cd.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include "macros.h"
 #include "built_in_cmd.h"
 #include "parse.h"
 
 void cd(struct command *cmd)
 {
-	printf("This is the cd command.\n");
+	//printf("This is the cd command.\n");
 	
-	if(parse != NULL)
-		chdir(parse(*input));
+	char *pth;
+	char path[MAX_COMMAND_LENGTH];
+	strcpy(path,pth);
+	char cwd[MAX_COMMAND_LENGTH];
+	getcwd(cwd,sizeof(cwd));
+	strcat(cwd,"/");
+	strcat(cwd,path);
+	printf("Current directory: %s\n", path);
+	int ret = chdir(path);
+	if(ret != 0)
+		printf("Error: Could not change to the directory \n");
 	else
-		chdir(getenv("HOME"));
+		system("pwd");
 	return;
 }
