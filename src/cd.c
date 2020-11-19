e47e9d086db59d7216a95f39b0c2f42c1e9ff672
jtw0229 <jasminewicker@my.unt.edu>
2020-11-19 10:04:04 -0600

    Implement built-in cd command

--- src/cd.c
+++ src/cd.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "built_in_cmd.h"
+#include "parse.h"
 
 void cd(struct command *cmd)
 {
 	printf("This is the cd command.\n");
+	
+	if(parse != NULL)
+		chdir(parse(*input));
+	else
+		chdir(getenv("HOME"));
+	return;
 }
