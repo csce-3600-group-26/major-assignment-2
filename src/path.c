fd52551af05d97a7ac1356752b3ce565b46ec603
hnn0038 <hanguyen6@my.unt.edu>
2020-11-21 02:22:18 -0600

    without arguments

--- src/path.c
+++ src/path.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "built_in_cmd.h"
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
 
 void path(struct command *cmd)
 {
-	printf("This is the path command.\n");
+	//printf("This is the path command.\n");
+	if (cmd->num_args == 1)
+	{
+	    printf("PATH : %s\n", gentenv("PATH"));
+	}
 }
