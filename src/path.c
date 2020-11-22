a68ca9cbcf0482359b360d593bbcd4ee144cdd1c
hnn0038 <hanguyen6@my.unt.edu>
2020-11-21 20:11:46 -0600

    add/delete path

--- src/path.c
+++ src/path.c
@@ -7,13 +7,8 @@
 #include <sys/wait.h>
 #include "string_util.h"
 
-#define MAX_LEN 1024;
-
 void path(struct command *cmd)
 {
-	//char cmdPath[MAX_LEN];
-	//char PATH[MAX_LEN];
-	
 	
 
 	if (cmd->num_args == 1)
@@ -25,14 +20,14 @@ void path(struct command *cmd)
 		if (strcmp(cmd->args[1],"+") == 0)
 		{
 			char *path_1;
+			
 			size_t path1_size = strlen(getenv("PATH")) + 1 + strlen(cmd ->args[2]) + 1;
 			path_1 = malloc(path1_size);
+			
 			strcpy(path_1,getenv("PATH"));
 			strcat(path_1, ":");
 			strcat(path_1, cmd->args[2]);
 			
-			//path_1 = strcat(getenv("PATH"), ":");
-			//path_1 = strcat(path_1,cmd->args[2]);
 			
 			setenv ("PATH",path_1, 1);
 		}
@@ -64,64 +59,4 @@ void path(struct command *cmd)
 		}
 	}
 	 
-
-
-
-
-
-
-	/*
-	
-	int execCmd (char *cmd, int argc, char **argv)
-	
-		char PATH[MAX_LEN];
-		char cmdPath[MAX_LEN];
-		pid_t child_pid;
-		int retStat;
-
-		printf ("cmd: %s\n", cmd);
-		printf ("PATH: %s\n", PATH);
-
-		if (cmd[0] == '/')
-			strcpy(cmdPath, cmd);
-		else if (cmd[0] == '.')
-		{
-			if (realpath(cmd, cmdPath) == NULL)
-			{
-				fprintf(stderr, " '%s' not found\n", cmd);
-				return 1;
-			}
-		} else {
-			strcpy(cmdPath, PATH);
-			strcat(cmdPath, "/");
-			strcat(cmdPath, cmd);
-		}
-		printf("cmdPath: %s\n", cmdPath);
-
-		if (access(cmdPath, F_OK) != 0)
-		{
-			fprintf(stderr, " '%s' not found\n", cmd);
-			return 1;
-		}
-		else {
-			child_pid = fork();
-			if (child_pid < 0)
-			{
-				perror("fork");
-				return -1;
-			}
-			else if (child_pid == 0)
-			{
-				if (execl(cmdPath,"","", (char *)0) == -1)
-				{
-					perror("execv");
-					return 1;
-				}
-			} else {
-				wait(&retStat);
-				return retStat;
-			}
-		}
-		return 1;
-	*/
 }
